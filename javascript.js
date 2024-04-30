abb381057e174b923738c16189311e903ebf71c3
ObedG45 <157981124+ObedG45@users.noreply.github.com>
2024-04-30 12:24:54 -0500

    Add function to know which one is better (rock paper or scissors), and to know which one won a point for it.

--- javascript.js
+++ javascript.js
@@ -1,49 +1,70 @@
-console.log("Hello gamers, welcome to the classic of Rock, Paper, Scissors! How may I help you today");
+console.log("Hello gamers, welcome to the classic of Rock, Paper, Scissors! You will be going against a computer to see who is a better rps competitor!");
+// Declare the variables of Rock, Paper, and Scissors.
+
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
 
 //Write a logical statement where the computer returns either rock, paper, or scissors.
-function getComputerChoice(){
-    let computerChoice = parseInt(Math.random() * 10); 
+    let computerChoice = parseInt(Math.random() * 10);
+function getComputerChoice(){ 
     if(computerChoice <= 3){
-        return "Rock";
+        return rock;
     }
     else if(computerChoice <=7){
-        return "Scissors";
-    } else {
-        return "Paper";
+        return scissors;
+    } else if(computerChoice <=10) {
+        return paper;
     }
 }
+
 console.log(getComputerChoice());
 
 
-// Write a prompt for the player (no case sensitive, and either rock, paper, or scissors), and also go back to the prompt if it's neither of the three.
-let choose = prompt("You pick: Rock, Paper, or Scissors");
-function chosen(choose){
-    return choose.charAt(0).toUpperCase() + choose.slice(1).toLowerCase();    
+// Write a prompt for the player (no case sensitive, and either rock, paper, or scissors).
+    let humanChoice = prompt("You pick: Rock, Paper, or Scissors.");
+function getHumanChoice(){
+    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
 }
-
-console.log(chosen(choose));
-choice();
-function choice(){
-if (chosen(choose) === "Rock"){
-    alert(`You have chosen ${choose}!`);
-} else if (chosen(choose) === "Scissors"){
-    alert(`You have chosen ${choose}. We'll see how this goes!`);
-} else if (chosen(choose) === "Paper"){
-    alert(`Interesting choice! You have chosen ${choose} this time`);
-} else {
-    //prompting again to choose one of the three (while trying to prevent an indefinite loop(which is happening right now))
-    alert("What is that Garbage?");
-   let choose = prompt("Let's do this again. Rock, Paper, or Scissors!");
-   console.log(chosen(choose));
-    choice();
-    
+function humanDeclaration(){
+if (humanChoice === "Rock"){
+    return rock;
+} else if (humanChoice === "Paper") {
+    return paper;
+} else if (humanChoice === "Scissors") {
+    return scissors;
 }
 }
-
-
+humanDeclaration();
+console.log(getHumanChoice());
 // Write the score system for both player and the computer (for everytime one of them has a point).
-// Write a function to play a round (to call playRound 5 times)
-function playRound(){
+let humanScore = 0;
+let computerScore = 0;
+// Write a function to play a round and declare a winner for each round (ongoing right now)
+function playRound(humanChoice, computerChoice){
+    rockPaperScissors(rock, paper, scissors);
+    if (computerChoice > humanChoice && humanChoice < computerChoice) {
+        alert ("Damn, that computer got a point for that");
+    } else if (humanChoice > computerChoice && computerChoice < humanChoice){
+        alert ("Good Job, you got a point for that, keep going to beating that dumb computer");
+    } else {
+        alert ("No points for either of you");
 }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 // Write a function that calls playRound function (PlayGame).
+function playGame(){
+
+}
 // Write a function that knows which weapon is better (Paper > Rock, Rock > Scissors, Scissors > Paper).
+
+function rockPaperScissors(rock, paper, scissors){
+    if (rock > scissors || scissors > paper || paper > rock){
+        return true;
+    } else {
+        return false;
+    }
+}
