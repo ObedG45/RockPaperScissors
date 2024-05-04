169e939fedfdcf40e2f8c6da5639da5353c74a44
ObedG45 <157981124+ObedG45@users.noreply.github.com>
2024-05-04 14:36:35 -0500

    Added the 'playGame();' function and put the playRound function, humanSelection, and computerSelection variables. Also updated the 'playRound();' function.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
-console.log("Hello gamers, welcome to the classic of Rock, Paper, Scissors! You will be going against a computer to see who is a better rps competitor!");
+alert("Hello gamers, welcome to the classic of Rock, Paper, Scissors! You will be going against a computer to see who is a better rps competitor!");
 // Declare the variables of Rock, Paper, and Scissors.
 
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
 
-//Write a logical statement where the computer returns either rock, paper, or scissors.
-    let computerChoice = parseInt(Math.random() * 10);
-function getComputerChoice(){ 
+//Write a logical statement where the computer returns either rock, paper, or scissors.   
+function getComputerChoice(){
+   let computerChoice = parseInt(Math.random() * 10);
     if(computerChoice <= 3){
         let computerChoice = rock;
         return rock;
@@ -20,68 +20,64 @@ function getComputerChoice(){
         return paper;
     }
 }
-
-console.log(getComputerChoice());
-
-
 // Write a prompt for the player (no case sensitive, and either rock, paper, or scissors).
-    let humanChoice = prompt("You pick: Rock, Paper, or Scissors.");
+    
 function getHumanChoice(){
+    let humanChoice = prompt("You pick: Rock, Paper, or Scissors.");
     return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+}    
+
+function humanDeclaration(humanChoice){
+    getHumanChoice();
+    if (humanChoice === "Rock"){
+        let humanChoice = rock;
+        return rock;
+    } else if (humanChoice === "Paper"){
+        let humanChoice = paper;
+        return paper;
+    } else if (humanChoice === "Scissors"){
+        let humanChoice = scissors;
+        return scissors;
+    }
 }
-function humanDeclaration(){
-if (humanChoice === "Rock"){
-    return rock;
-} else if (humanChoice === "Paper") {
-    return paper;
-} else if (humanChoice === "Scissors") {
-    return scissors;
-}
-}
-humanDeclaration();
-console.log(getHumanChoice());
 // Write the score system for both player and the computer (for everytime one of them has a point).
 let humanScore = 0;
 let computerScore = 0;
 // Write a function to play a round and declare a winner for each round (while making all the string values true) (solved) 
 function playRound(humanChoice, computerChoice){
-    if (humanChoice === computerChoice){
-        alert("No points for either of you.");
+    if ((humanChoice === rock) && (computerChoice === rock)){
+        alert(`No points for either of you. Computer picked ${computerChoice} as well.`);
+    } else if ((humanChoice === paper) && (computerChoice === paper)){
+        alert(`No points for either of you. Computer picked ${computerChoice} as well.`);
+    } else if ((humanChoice === scissors) && (computerChoice === scissors)){
+        alert (`No points for either of you. Computer picked ${computerChoice} as well.`);
     } else if ((humanChoice === rock) && (computerChoice === scissors)){
-        alert("Good Job, you got a point from that dumb computer!");
+        alert(`Good Job, you got a point from that dumb computer! Computer picked ${computerChoice}`);
         humanScore++;
     } else if ((humanChoice === scissors) && (computerChoice === rock)){
-        alert ("Damn, that computer got a point from you.");
+        alert (`Damn, that computer got a point from you. Computer picked ${computerChoice}`);
         computerScore++;
     } else if ((humanChoice === paper) && (computerChoice=== rock)){
-        alert ("Good Job, you got a point from that dumb computer!");
+        alert (`Good Job, you got a point from that dumb computer! Computer picked ${computerChoice}`);
         humanScore++;
     } else if ((humanChoice === rock) && (computerChoice === paper)){
-        alert("Damn, that computer got a point from you.");
+        alert(`Damn, that computer got a point from you. Computer picked ${computerChoice}`);
         computerScore++;
-    } else if (humanChoice > computerChoice){
-        alert("Good Job, you got a point from that dumb computer!");
+    } else if ((humanChoice === scissors) && (computerChoice === paper)){
+        alert(`Good Job, you got a point from that dumb computer! Computer picked ${computerChoice}`);
         humanScore++;
-    } else {
-        alert("Damn, that computer got a point from you.");
+    } else if ((humanChoice === paper) && (computerChoice === scissors)){
+        alert(`Damn, that computer got a point from you. Computer picked ${computerChoice}`);
         computerScore++;
     }
 
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
 // Write a function that calls playRound function (PlayGame).
 function playGame(){
-
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+    alert(`So far, computer has ${computerScore} points and you have ${humanScore} points.`);
 }
-// Write a function that knows which weapon is better (Paper > Rock, Rock > Scissors, Scissors > Paper).
 
-function rockPaperScissors(){
-    if (rock < scissors && scissors > paper && paper < rock){
-        return true;
-    } else {
-        return false;
-    }
-}
